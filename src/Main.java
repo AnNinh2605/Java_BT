22ff32c1dacab845af054a0ac8b4c7c4c3be096d
AnNinh2605 <anninh1511@gmail.com>
2024-01-10 21:11:39 +0700

    replace recrusive by while loop

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,21 @@ public class Main {
     public static void confirmPassword() {
         System.out.print("Enter your password: ");
         Scanner sc = new Scanner(System.in);
-        String password=sc.nextLine();
-        if (checkPassword(password)){
-            System.out.println("Valid password");
+        String password = sc.nextLine();
+//        if (checkPassword(password)){
+//            System.out.println("Valid password");
+//        }
+//        else {
+//                System.out.print("Enter your password: ");
+//                password = sc.nextLine();
+//                confirmPassword();
+//        }
+        while (!checkPassword(password)) {
+            System.out.println("Invalid password: ");
+            System.out.print("Enter your password: ");
+            password = sc.nextLine();
         }
-        else {
-                System.out.print("Enter your password: ");
-                password = sc.nextLine();
-                confirmPassword();
-        }
+        System.out.println("Valid password");
     }
     public static boolean checkPassword(String password) {
         if (password.length() >= 8 && checkLetter(password) && checkNumber(password) && checkSpecialDigit(password)){
