c22f206a1e3a85f0b03dca3eee0f024333308d05
AnNinh2605 <anninh1511@gmail.com>
2024-01-12 23:22:11 +0700

    flag variable

--- src/GuessRandom.java
+++ src/GuessRandom.java
@@ -5,48 +5,47 @@ public class GuessRandom {
         int guessNum = 1;
         int floor = 1;
         int ceiling = 100;
+        boolean flag = false;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your guess:  ");
-        int guess = sc.nextInt();
-
-        while ((guess < 1) || (guess > 100)) {
-            System.out.print("Your guess must between 1 and 100: ");
-            guess = sc.nextInt();
-        }
         int random = (int)(Math.random()*100 + 1);
 //        System.out.println("Random is: " +random);
 
-        while (random != guess) {
+        while (!flag) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your guess:  ");
+            int guess = sc.nextInt();
+
+            while ((guess < 1) || (guess > 100)) {
+                System.out.print("Your guess must between 1 and 100: ");
+                guess = sc.nextInt();
+            }
             if (random > guess) {
                 floor = guess;
                 System.out.println("Your number is lower than the one I picked, It ranges between " + floor + " and " + ceiling);
             }
-            else{
+            else if (random < guess){
                 ceiling = guess;
                 System.out.println("Your number is higher than the number I picked, It ranges between " + floor + " and " + ceiling);
             }
-            System.out.print("Enter your guess: ");
-            guess = sc.nextInt();
+            else {
+                System.out.println("Correct! You guessed the number " + guess + " in " + guessNum + " tries.");
+                 flag = !flag;
+            }
             guessNum ++;
         }
-        System.out.println("Correct! You guessed the number " + guess + " in " + guessNum + " tries.");
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
         System.out.println("\n" +
                 "Welcome to the Number Guessing Game!\n" +
                 "I have selected a random number between 1 and 100.\n" +
                 "Try to guess what it is!");
-
-        playGame();
-        System.out.print("Do you want to play again? (yes/no): ");
-        String nextPlay = sc.nextLine();
-
-        while (nextPlay.equals("yes")) {
+        while (flag) {
             playGame();
+            Scanner sc = new Scanner(System.in);
             System.out.print("Do you want to play again? (yes/no): ");
-            nextPlay =  sc.nextLine();
+            String nextPlay = sc.nextLine();
+            flag = nextPlay.equals("yes");
         }
     }
 }
