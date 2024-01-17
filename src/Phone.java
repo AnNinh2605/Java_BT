6f6742cd26268c520bc0ccde79ffe4fc5c63d19a
AnNinh2605 <anninh1511@gmail.com>
2024-01-17 23:11:47 +0700

    Phone number create

---
+++ src/Phone.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Phone {
+
+    // validate name
+    static boolean checkName(String name) {
+        for (char charName : name.toCharArray()) {
+            if (!Character.isLetter(charName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean flag = false;
+        Scanner sc = new Scanner((System.in));
+        ArrayList<String> nameArrayList = new ArrayList<String>();
+        ArrayList<Integer> phoneArrayList = new ArrayList<Integer>();
+
+        while (!flag) {
+            System.out.print("==================================\n" +
+                    "WELCOME TO CONTACT MANAGEMENT\n" +
+                    "==================================\n" +
+                    "1. Create Contact\n" +
+                    "2. View Contact\n" +
+                    "3. Exit\n" +
+                    "(Choose 1 to create contact, 2 to view contact, 3 to exit program) : ");
+
+            int select = sc.nextInt();
+            sc.nextLine();
+            while (select < 1 || select > 3) {
+                System.out.println("Choose 1 to create contact, 2 to view contact, 3 to exit program) : ");
+                select = sc.nextInt();
+                sc.nextLine();
+            }
+            if (select == 1) {
+                System.out.print("Name : ");
+                String name = sc.nextLine();
+                while (!checkName(name)) {
+                    System.out.print("Name contains only digits: ");
+                    name = sc.nextLine();
+                }
+                nameArrayList.add(name);
+
+                System.out.print("Phone Number : ");
+                int phoneNumber = sc.nextInt();
+                phoneArrayList.add(phoneNumber);
+
+                System.out.println("Created sucessful");
+            } else if (select == 2) {
+                for (int index = 0; index < nameArrayList.size(); index++) {
+                    System.out.println("Name: " + nameArrayList.get(index) + "\n" +
+                            "Phone number: " + phoneArrayList.get(index));
+                }
+                sc.nextLine();
+            } else {
+                flag = true;
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+
+        System.out.println("Sleep time in ms = " + (System.currentTimeMillis() - start));
+    }
+}
