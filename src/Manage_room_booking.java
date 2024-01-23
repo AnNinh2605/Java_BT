d0324fef1d3be1fbb019ea216d444e29923063c3
AnNinh2605 <anninh1511@gmail.com>
2024-01-23 11:05:21 +0700

    <feature> check name && input date time

--- src/Manage_room_booking.java
+++ src/Manage_room_booking.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Manage_room_booking {
@@ -8,6 +9,9 @@ public class Manage_room_booking {
         ArrayList<String> nameArrayList = new ArrayList<String>();
         ArrayList<String> checkInTimeArrayList = new ArrayList<String>();
         ArrayList<String> checkOutTimeArrayList = new ArrayList<String>();
+        ArrayList<Integer> dayArrayList = new ArrayList<Integer>();
+        ArrayList<Integer> monthArrayList = new ArrayList<Integer>();
+        ArrayList<Integer> yearArrayList = new ArrayList<Integer>();
 
         while (true) {
             System.out.print(
@@ -26,24 +30,51 @@ public class Manage_room_booking {
             }
             if (select == 1) {
                 System.out.println("Your selected Room Booking");
-                System.out.print("Name: ");
-                String name = sc.nextLine();
-                nameArrayList.add(name);
+                String name;
+                while (true) {
+                    System.out.print("Name: ");
+                    name = sc.nextLine();
+                    if (checkName(name)) {
+                        nameArrayList.add(name);
+                        break;
+                    } else {
+                        System.out.println("Name only letters");
+                    }
+                }
+
+                // day month year input
+                System.out.print("Day: ");
+                int day = sc.nextInt();
+                System.out.print("Month: ");
+                int month = sc.nextInt();
+                System.out.print("Year: ");
+                int year = sc.nextInt();
+
+                Calendar getdate = Calendar.getInstance();
+                getdate.set(year, month - 1, day);
+
+                int getYear = getdate.get(Calendar.YEAR);
+                int getMonth = getdate.get(Calendar.MONTH) + 1;
+                int getday = getdate.get(Calendar.DAY_OF_MONTH);
+                dayArrayList.add(getday);
+                monthArrayList.add(getMonth);
+                yearArrayList.add(getYear);
 
                 System.out.print("Check-in time: ");
-                String checkInTime = sc.nextLine();
+                String checkInTime = new Scanner(System.in).nextLine();
                 checkInTimeArrayList.add(checkInTime);
 
                 System.out.print("Check-out time: ");
-                String checkoutTime = sc.nextLine();
+                String checkoutTime = new Scanner(System.in).nextLine();
                 checkOutTimeArrayList.add(checkoutTime);
                 System.out.println("Create successful");
             } else if (select == 2) {
                 System.out.println("Display Booking List");
                 for (int i = 0; i < nameArrayList.size(); i++) {
                     System.out.println("Name: " + nameArrayList.get(i) + "\n" +
-                            "Check in time " + checkInTimeArrayList.get(i) + "\n" +
-                            "Check out time " + checkOutTimeArrayList.get(i));
+                            "Booking day: " + dayArrayList.get(i) + "/" + monthArrayList.get(i) + "/" + yearArrayList.get(i)+"\n" +
+                            "Check in time: " + checkInTimeArrayList.get(i) + "\n" +
+                            "Check out time: " + checkOutTimeArrayList.get(i));
                 }
             } else if (select == 3) {
                 System.out.println("Calculate Minimum Number of Rooms Required");
@@ -53,5 +84,15 @@ public class Manage_room_booking {
             }
         }
         ;
+    }
+
+    //check name only letter
+    public static boolean checkName(String name) {
+        for (char check : name.toCharArray()) {
+            if (!Character.isLetter(check)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
